19c1d8c5cca89bf194ad70305eb275fd3da4fc4c
Bang Pham Huu <b.phamhuu@jacobs-university.de>
2025-05-09 08:49:48 +0200

    Fixed toolbar

--- wcps_client.py
+++ wcps_client.py
@@ -67,9 +67,6 @@ class wcps_client:
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&WcpsClient1')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'WcpsClient1')
-        self.toolbar.setObjectName(u'WCPSClient')
 
 
     # noinspection PyMethodMayBeStatic
@@ -88,81 +85,6 @@ class wcps_client:
         return QCoreApplication.translate('wcps_client', message)
 
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-            print("added to toolbar")
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-            print("added to menu")
-
-        self.actions.append(action)
-
-        return action
-
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
@@ -171,11 +93,16 @@ class wcps_client:
         # pydevd_pycharm.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
 
         icon_path = f'{self.plugin_dir}/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'WCPS 1.1 Client'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        self.action = QAction(QIcon(icon_path), 'WCPS 1.1 Client', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+
+        # Add icon to main QGIS toolbar
+        self.iface.addToolBarIcon(self.action)
+
+        # Add WCPSClient as an item of Plugins dropdown menu
+        self.iface.addPluginToMenu(
+            self.menu,
+            self.action)
 
         # will be set False in run()
         self.first_start = True
